412c072b04818a3ed8d58622c92e793d86baa21f
William Forsberg <vilfor@hotmail.se>
2023-04-01 15:22:27 +0200

    now with more beams

--- Assets/Scripts/LineTo.cs
+++ Assets/Scripts/LineTo.cs
@@ -2,20 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AimAt : MonoBehaviour
+public class LineTo : MonoBehaviour
 {
     public GameObject toolBase;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
-
 
     // Update is called once per frame
     void Update()
     {
-        gameObject.transform.LookAt(toolBase.transform.GetChild(0));
+        gameObject.GetComponent<LineRenderer>().SetPosition(0, gameObject.transform.position);
+        gameObject.GetComponent<LineRenderer>().SetPosition(1, toolBase.transform.GetChild(0).position);
     }
 }
+
